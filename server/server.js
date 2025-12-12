f4ef9a1948d77aa9fed4fcaabaf7d3ae21e418b0
Muhammed Anas K <muhammedanask247@gmail.com>
2025-12-12 06:00:00 +0530

    fix cors 3

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('(.*)', cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(hpp());
 app.use(sanitizeMiddleware);
